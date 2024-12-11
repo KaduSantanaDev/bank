21872a38e265883679cb4666e10682f9151192e9
kadu <kaduzera.ks@gmail.com>
2024-12-11 18:41:24 -0300

    feat: add createUser method for users controller

--- api/server.go
+++ api/server.go
@@ -21,13 +21,15 @@ func NewServer(store *db.Store) *Server {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
+	router.POST("/users/", server.createUser)
+
+	router.POST("/accounts/", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 
-	router.POST("/transfers", server.createTransfer)
+	router.POST("/transfers/", server.createTransfer)
 
 	server.router = router
 	return server
