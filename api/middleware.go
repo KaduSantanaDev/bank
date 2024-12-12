d66ad180e84bcb76bd7b7f6435641af9c1ee2b5c
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-12 19:09:25 -0300

    feat: add authentication middleware and protect routes
    Introduce authMiddleware in `middleware.go` to validate tokens for secured access. Update `server.go` to apply the middleware to protected routes, ensuring only authenticated users can access account and transfer operations.

---
+++ api/middleware.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"bank/token"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+const (
+	authorizationHeaderKey  = "Authorization"
+	authorizationTypeBearer = "Bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+
+		if authorizationType != authorizationTypeBearer {
+			err := errors.New("invalid authorization type")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authorizationPayloadKey, payload)
+		ctx.Next()
+	}
+}
